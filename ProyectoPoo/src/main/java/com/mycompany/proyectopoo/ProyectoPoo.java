f3abd210e06c7a63fed3fa17fa05357d04712a19
Usuario <Usuario@Usuario-PC>
2022-06-25 15:45:40 -0500

    probanding

--- ProyectoPoo/src/main/java/com/mycompany/proyectopoo/ProyectoPoo.java
+++ ProyectoPoo/src/main/java/com/mycompany/proyectopoo/ProyectoPoo.java
@@ -12,6 +12,6 @@ package com.mycompany.proyectopoo;
 public class ProyectoPoo {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("porquenosirve");
     }
 }
