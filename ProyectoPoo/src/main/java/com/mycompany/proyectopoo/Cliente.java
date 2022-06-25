b5def2c133d3f8b476a9580275403b7d5d5788f4
bsjalon <bsjalon@espol.edu.ec>
2022-06-25 16:31:36 -0500

    probanding

---
+++ ProyectoPoo/src/main/java/com/mycompany/proyectopoo/Cliente.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectopoo;
+
+/**
+ *
+ * @author Usuario
+ */
+public class Cliente extends Persona {
+    private String datosRepresentante;
+    
+    public String getDatosRepresentante(){
+        return datosRepresentante;
+    }
+    public void setDatosRepresentante(String datosRepresentante){
+        this.datosRepresentante = datosRepresentante;
+    }
+    
+}
