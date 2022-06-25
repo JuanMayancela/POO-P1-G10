cb9db675aec5ebd9cfb67de5877180e41e9ce03a
Lenovo <Lenovo@DESKTOP-55FA2C6>
2022-06-25 16:47:32 -0500

    se añadio los getters y setters a la clase Persona

--- ProyectoPoo/src/main/java/com/mycompany/proyectopoo/Persona.java
+++ ProyectoPoo/src/main/java/com/mycompany/proyectopoo/Persona.java
@@ -12,5 +12,38 @@ public class Persona {
     private String nombre, email;
     private char cedula, telefono;
     
+    //Getters y Setters
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public char getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(char cedula) {
+        this.cedula = cedula;
+    }
+
+    public char getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(char telefono) {
+        this.telefono = telefono;
+    }
     
 }
