7a987fa224ee5ac67df1f2d9e6f89382d6bfe268
mamay <mamay@192.168.200.13>
2022-06-25 17:11:38 -0500

    Clase sistema

---
+++ ProyectoPoo/src/main/java/com/mycompany/proyectopoo/Sistema.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectopoo;
+
+/**
+ *
+ * @author JuanMayancela
+ */
+public class Sistema {
+    
+    //Metodo inicializar sistema
+    public void inicializarSistema(){
+    
+    }
+    
+    //Metodo salir del sistema
+    public void salir(){
+        System.out.println("Gracias por su visita");
+    
+    }
+}
