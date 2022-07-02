ce5c91e68d87b5fad2bd3c03d8629f10734f176c
Lenovo <Lenovo@DESKTOP-55FA2C6>
2022-07-02 18:19:01 -0500

    se creo el MenuAtencion

---
+++ ProyectoPoo/src/main/java/com/mycompany/proyectopoo/MenuAtencion.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.proyectopoo;
+
+/**
+ *
+ * @author ArianaGonzabay
+ */
+public enum MenuAtencion{
+    Registrar_Atencion, 
+    Consultar_Atencion
+}
