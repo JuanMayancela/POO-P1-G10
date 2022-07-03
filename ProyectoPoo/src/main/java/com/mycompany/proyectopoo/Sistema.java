9f7dda22df1204a51b0c31ccd806d37707717dd4
ArianaGonzabay <108005634+ArianaGonzabay@users.noreply.github.com>
2022-07-02 19:37:07 -0500

    Update Sistema.java

--- ProyectoPoo/src/main/java/com/mycompany/proyectopoo/Sistema.java
+++ ProyectoPoo/src/main/java/com/mycompany/proyectopoo/Sistema.java
@@ -9,9 +9,16 @@ package com.mycompany.proyectopoo;
  * @author JuanMayancela
  */
 public class Sistema {
+    public static ArrayList<Empleado> empleados;
+    public static ArrayList<Cliente> clientes;
+    public static ArrayList<Servicio> servicios;
     
     //Metodo inicializar sistema
-    public void inicializarSistema(){
+    public static void inicializarSistema(){
+        //Empleado
+        empleados = new ArrayList<>();
+        Empleado e1 = new Empleado("0926441132", "Juan", "0985470321", "juangp20@gmail.com", "Activo");
+        empleados.add(e1);
     
     }
     
