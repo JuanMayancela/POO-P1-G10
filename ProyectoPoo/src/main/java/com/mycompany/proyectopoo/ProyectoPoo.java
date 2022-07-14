967e80189ad6681e5cd4f33ee664ec986515d053
bsjalon <108003043+bsjalon@users.noreply.github.com>
2022-07-13 22:13:54 -0500

    Update ProyectoPoo.java

--- ProyectoPoo/src/main/java/com/mycompany/proyectopoo/ProyectoPoo.java
+++ ProyectoPoo/src/main/java/com/mycompany/proyectopoo/ProyectoPoo.java
@@ -28,32 +28,14 @@ public class ProyectoPoo {
         //opcion 1 Servicios
         switch(opcion){
             case "1": Servicio.);
-                //Recorro la lista con los objetos de clase Servicio
-                for(Servicio servicio: servicios){
-                    System.out.println(servicio);//muestro cada uno de los servicios
-                }
+                
         }
     }
     
 
-    //opcion 2 Empleados
-    if(opcion == "2"){
-        //Recorro la lista con los objetos de clase Empleado
-        for(Empleado empleado: empleados){
-            System.out.println(empleado);//muestro cada uno de los empleados
-        }
-        
-    }
-    //opcion 3 Clientes
-    else if(opcion == "3"){
-        //Recorro la lista con los objetos de clase Cliente
-        for(Cliente cliente: clientes){
-            System.out.println(cliente);//muestro cada uno de los clientes
-        }
-        
     }
     //opcion 4 Citas
-    else if(opcion == "4"){
+    if(opcion == "4"){
       System.out.println("""
                          1. Crear Cita
                          2. Eliminar Cita
@@ -62,7 +44,7 @@ public class ProyectoPoo {
 
     
     //opcion 5 Atenciones
-    else if(opcion == "5"){
+    if(opcion == "5"){
       System.out.println("""
                          1. Registrar atención
                          2. Consultar atención""");
@@ -70,7 +52,7 @@ public class ProyectoPoo {
     
     
     //opcion 6 Salir
-    else if(opcion == "6"){
+    if(opcion == "6"){
       System.out.println("Gracias por su visita");
     }
     input.close();
